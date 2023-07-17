22a053f8d4723a1aada4723e7b5049281be69771
Ajay Singh <ajay87891@gmail.com>
2023-07-17 15:14:11 +0530

    set navigation

--- components/navbar.js
+++ components/navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
   const [toggel, setToggel] = useState(false);
   return (
     
-    <nav className="w-full flex justify-between items-center  bg-white/20 fixed top-0 backdrop-blur-sm px-10 z-40 py-4">
+    <nav className="w-full flex justify-between items-center  bg-primary fixed top-0 backdrop-blur-sm px-10 z-40 py-4">
       <div className="flex justify-center items-center  ">
         <img src={images.logo.src} alt="ksd" className="max-w-[100px] 2xl:max-w-[220px]" />
       </div>
@@ -20,8 +20,8 @@ const Navbar = () => {
           (item) => {
             return (
               <li className="cursor-pointer flex flex-col group items-center" key={`link-${item}`}>
-                <div className="w-2 h-2 bg-transparent  mb-1 rounded-full group-hover:bg-violet-700" />
-                <a className="text-violet-500 font-medium 2xl:text-2xl font-Quicksand group-hover:text-violet-700 ease-in-out duration-500 transition-all uppercase" href={`#${item}`}>{item}</a>
+                <div className="w-2 h-2 bg-transparent  mb-1 rounded-full group-hover:bg-violet-200" />
+                <a className="text-white font-medium 2xl:text-2xl font-Quicksand group-hover:text-violet-200 ease-in-out duration-500 transition-all uppercase" href={`#${item}`}>{item}</a>
               
               </li>
             );
@@ -34,7 +34,7 @@ const Navbar = () => {
         <svg
                 className="w-6 h-6"
                 aria-hidden="true"
-                fill="currentColor"
+                fill="white"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
               >
@@ -45,7 +45,7 @@ const Navbar = () => {
                 ></path>
               </svg></button>
         {
-          toggel ? (<motion.div className=" opacity-0 fixed z-50 top-0 bottom-0 right-0 p-4 w-[80%] h-[100vh] flex  flex-col  bg-violet-100 shadow-lg bg-cover bg-repeat "
+          toggel ? (<motion.div className=" opacity-0 fixed z-50 top-0 bottom-0 right-0 p-4 w-[80%] h-[100vh] flex  flex-col  bg-primary shadow-lg bg-cover bg-repeat "
           style={{
             //backgroundImage: `url(${bg.src})`
           }}
@@ -53,14 +53,14 @@ const Navbar = () => {
            transition={{duration:0.75,ease:'easeOut'}}
           
           >
-            <button onClick={()=> setToggel(false)}><HiX  className="text-violet-700  h-9 w-9 absolute right-5" /></button>
+            <button onClick={()=> setToggel(false)}><HiX  className="text-white  h-9 w-9 absolute right-5" /></button>
             <ul className="flex flex-col space-y-8 mt-8">
 
             {menuItems.map(
           (item) => {
             return (
               <li key={item}>
-                <a className="text-violet-500 text-2xl cursor-pointer font-medium font-Quicksand hover:text-violet-700 ease-in-out duration-500 transition-all uppercase" href={`#${item}`} onClick={()=> setToggel(false)}>{item}</a>
+                <a className="text-white text-2xl cursor-pointer font-medium font-Quicksand hover:text-purple-500 ease-in-out duration-500 transition-all uppercase" href={`#${item}`} onClick={()=> setToggel(false)}>{item}</a>
               </li>
             );
           }
