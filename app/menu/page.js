afd34d79534e564c195bccf9c614c68086e30fc2
Ajay Singh <ajay87891@gmail.com>
2023-07-20 17:06:59 +0530

    fixed cards

---
+++ app/menu/page.js
@@ -0,0 +1,12 @@
+import PagesNavbar from "@/components/PagesNavBar";
+
+export default function Menu() {
+  return (
+    <>
+      <PagesNavbar></PagesNavbar>
+      <div className=" pt-24 lg:pt-32 bg-cover bg-repeat bg-center relative  flex items-center justify-start min-w-screen flex-col min-h-screen">
+        <h1>Menu Page</h1>
+      </div>
+    </>
+  );
+}
