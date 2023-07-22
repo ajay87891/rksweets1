0d8ad200d8b1b1401321b84569ee3610f11ce4f2
Ajay Singh <ajay87891@gmail.com>
2023-07-22 20:56:37 +0530

    updated full menu

--- app/fullmenu/page.js
+++ app/fullmenu/page.js
@@ -3,6 +3,11 @@
 
 import PagesNavbar from "@/components/PagesNavBar";
 import Image from 'next/image'
+import a from '../../public/assets/1.webp'
+import b from '../../public/assets/2.webp'
+import c from '../../public/assets/3.webp'
+import d from '../../public/assets/4.webp'
+
 
 
 
@@ -18,7 +23,16 @@ export default function Page() {
       <PagesNavbar></PagesNavbar>
 
       <div className=" pt-24 lg:pt-32 ">
-        <h1 className="text-primary font-Shrikhand text-3xl md:text-5xl lg:text-6xl 2xl:text-7xl">Full menu</h1>
+        <h1 className="text-primary font-Shrikhand text-3xl md:text-5xl lg:text-6xl 2xl:text-7xl text-center">Full menu</h1>
+        <div className="w-screen flex flex-col">
+            <Image src={a} />
+            <Image src={b} />
+            <Image src={c} />
+            <Image src={d} />
+
+
+        </div>
+        
       </div>
     </>
   );
