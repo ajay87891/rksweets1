f06fb924250238d66a4db0e9ed8d434674561c06
Ajay Singh <ajay87891@gmail.com>
2023-07-22 20:20:27 +0530

    added full menu

---
+++ app/fullmenu/page.js
@@ -0,0 +1,25 @@
+
+'use client'
+
+import PagesNavbar from "@/components/PagesNavBar";
+import Image from 'next/image'
+
+
+
+export default function Page() {
+  
+  
+
+    
+    
+ 
+  return (
+    <>
+      <PagesNavbar></PagesNavbar>
+
+      <div className=" pt-24 lg:pt-32 ">
+        <h1 className="text-primary font-Shrikhand text-3xl md:text-5xl lg:text-6xl 2xl:text-7xl">Full menu</h1>
+      </div>
+    </>
+  );
+}
--- app/menu/page.js
+++ app/menu/page.js
@@ -16,17 +16,21 @@ export default function Menu() {
   return (
     <>
       <PagesNavbar></PagesNavbar>
-
-      <div className=" mt-24 lg:mt-32 flex justify-center   max-w-screen  min-h-screen">
-        <div className="grid grid-cols-1 sm:grid-cols-2    xl:grid-cols-3  auto-cols-max mx-auto">
+      <div className="mt-24 lg:mt-32 flex justify-center items-center flex-col">
+        <h1 className="text-primary font-Shrikhand text-3xl md:text-5xl lg:text-6xl 2xl:text-7xl">
+          Menu
+        </h1>
+        <p className="font-Kalam text-xl md:text-3xl lg:text-4xl 2xl:text-5xl">Please Select from categories below</p>
+      </div>
+      <div className="  flex justify-center   max-w-screen  min-h-screen">
+        <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  auto-cols-max mx-auto">
           {category?.map((cat, index) => {
             return (
               <div key={index} className="">
-                
                 <CardCategory
                   slug={cat.slug}
                   title={cat.name}
-                  image={cat.imageurl?urlFor(cat.imageurl):null}
+                  image={cat.imageurl ? urlFor(cat.imageurl) : null}
                 />
               </div>
             );
